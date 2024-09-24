34af21eb73d00cf10fca7633e400192f2e22f1f9
jsmethie <jsmethie@terraframe.com>
2024-09-24 12:37:31 -0600

    chore: docker build setup

---
+++ src/build/docker/know-stac/release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#
+#
+
+# Run with elevated 'sudo' permissions as necessary
+
+set -e
+
+echo $DOCKER_HUB_TFBUILDER_PSW | docker login --username $DOCKER_HUB_TFBUILDER_USR --password-stdin
+
+docker push terraframe/knowstac --all-tags
