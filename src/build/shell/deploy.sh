e88e9e996f51adfea9b3fca71766b1ada2b2f6e3
Richard Rowlands <rrowlands@terraframe.com>
2025-12-08 11:45:20 -0700

    Knowstac deployment scripts

---
+++ src/build/shell/deploy.sh
@@ -0,0 +1,66 @@
+#
+# Copyright 2025 The Department of Interior
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This tells the build which version of npm to use:
+. $NVM_DIR/nvm.sh && nvm install lts/jod
+
+:
+: ----------------------------------
+:  CONFIGURE  
+: ----------------------------------
+:
+
+export MAVEN_OPTS="-Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.resolver.transport=wagon"
+export ANSIBLE_HOST_KEY_CHECKING=false
+export NODE_OPTIONS="--max_old_space_size=1500"
+
+export DOCKER_CLIENT_TIMEOUT=120
+export COMPOSE_HTTP_TIMEOUT=120
+
+:
+: ----------------------------------
+:  BUILD
+: ----------------------------------
+:
+
+if [ "$build_artifact" == "true" ]; then
+  cd $WORKSPACE/uasdm
+  mvn clean install -B
+fi
+
+:
+: ----------------------------------
+:  DEPLOY
+: ----------------------------------
+:
+
+if [ "$deploy" == "true" ]; then
+  # Build a Docker image
+  cd $WORKSPACE/knowstac/src/build/docker/know-stac/api-web && ./build.sh
+  cd $WORKSPACE/knowstac/src/build/docker/know-stac/ui-web && ./build.sh
+
+  # Run Ansible deploy
+  cd $WORKSPACE/geoprism-cloud/ansible
+
+  [ -h ./inventory ] && unlink ./inventory
+  [ -d ./inventory ] && rm -r ./inventory
+  ln -s $WORKSPACE/geoprism-platform/ansible/inventory ./inventory
+
+  [ -h ../permissions ] && unlink ../permissions
+  ln -s $WORKSPACE/geoprism-platform/permissions ../permissions
+
+  ansible-playbook -v -i ./inventory/knowstac/$environment.ini ./knowstac.yml --extra-vars "clean_db=$clean_db clean_solr=$clean_solr clean_orientdb=$clean_orientdb elasticsearch_clean=$elasticsearch_clean webserver_docker_image_tag=$tag docker_image_path=../../knowstac/src/build/docker/know-stac/api-web/target/knowstac.dimg.gz docker_image_path2=../../knowstac/src/build/docker/know-stac/ui-web/target/knowstac-ui.dimg.gz"
+fi
---
+++ src/build/shell/release.sh
@@ -0,0 +1,147 @@
+#
+# Copyright 2020 The Department of Interior
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#if curl -f -s --head "https://dl.cloudsmith.io/public/terraframe/osmre-uav/maven/gov/osmre/uasdm/uasdm-server/$CGR_RELEASE_VERSION/uasdm-server-$CGR_RELEASE_VERSION.jar" | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null; then
+#    echo "The release version $IDM_VERSION has already been deployed! Please ensure you are releasing the correct version."
+#    exit 1
+#fi
+
+git config --global user.name "$GIT_TF_BUILDER_USERNAME"
+git config --global user.email builder@terraframe.com
+
+. $NVM_DIR/nvm.sh && nvm install lts/jod
+
+if [ "$release_uasdm" == "true" ]; then
+  ## Update IDM Version in System Component and Commit Compiled NodeJS Source
+  cd $WORKSPACE/uasdm
+  
+  git checkout $release_branch
+  git pull
+  sed -i -E "s_<span id=\"automated-version-replace\">.*</span>_<span id=\"automated-version-replace\">$IDM_VERSION</span>_g" uasdm-ui/src/app/admin/component/system/system-info.component.html
+  cd uasdm-ui
+  npm install --force
+  node -v && npm -v
+  #node --max_old_space_size=4096 ./node_modules/webpack/bin/webpack.js --config config/webpack.prod.js --profile
+  npm run build
+  
+  cd $WORKSPACE/uasdm
+  git add -A
+  git diff-index --quiet HEAD || git commit -m 'Preparing for release'
+  if [ "$dry_run" == "false" ]; then
+    git push
+  else
+    git reset --hard
+    git clean -fdx
+  fi
+  
+  ## License Headers
+  cd $WORKSPACE/uasdm
+  git checkout $release_branch
+  mvn license:format -B
+  git add -A
+  git diff-index --quiet HEAD || git commit -m 'License headers'
+  if [ "$dry_run" == "false" ]; then
+    git push
+  else
+    git reset --hard
+    git clean -fdx
+  fi
+  
+  # Release
+  cd $WORKSPACE/uasdm
+  git checkout $release_branch
+  mvn release:prepare -B -DdryRun=$dry_run -Dtag=$IDM_VERSION \
+                   -DreleaseVersion=$IDM_VERSION \
+                   -DdevelopmentVersion=$IDM_NEXT
+                   
+  mvn release:perform -B -DdryRun=$dry_run -Darguments="-Dmaven.javadoc.skip=true -Dmaven.site.skip=true"
+  
+  # Generate Changelog
+  cd $WORKSPACE/uasdm
+  if [ "$dry_run" == "true" ]; then
+    git tag $IDM_VERSION
+  fi
+  sed -i "0,/<version>.*<\/version>/s//<version>$IDM_VERSION<\/version>/" pom.xml
+  mvn git-changelog-maven-plugin:git-changelog --non-recursive
+  mv CHANGELOG.md CHANGELOG-old.md
+  head -n 3 CHANGELOG-old.md > CHANGELOG.md
+  echo "" >> CHANGELOG.md
+  cat CHANGELOG2.md >> CHANGELOG.md
+  tail -n +3 CHANGELOG-old.md >> CHANGELOG.md
+  git add CHANGELOG.md
+  git commit --no-verify -m "chore(release): update changelog for $IDM_VERSION"
+  if [ "$dry_run" == "false" ]; then
+    git push
+  else
+    echo "Generated changelog as:"
+    cat CHANGELOG.md
+    git reset --hard
+  fi
+fi
+
+if [ "$release_docker" == "true" ]; then
+  cd $WORKSPACE/uasdm/src/build/docker/uasdm
+  export tag=$IDM_VERSION
+  ./build.sh
+  
+  if [ "$dry_run" == "false" ]; then
+    ./release.sh
+  fi
+fi
+
+if [ "$tag_platform" == "true" ]; then
+  cd $WORKSPACE/geoprism-platform
+  
+  git checkout master
+  git merge origin/dev
+  
+  if [ "$dry_run" == "false" ]; then
+    git push
+    git tag -a idm-$IDM_VERSION -m "Deployment scripts for IDM version $IDM_VERSION"
+    git push origin idm-$IDM_VERSION
+  else
+    git tag -a idm-$IDM_VERSION -m "Deployment scripts for IDM version $IDM_VERSION"
+  fi
+fi
+
+if [ "$tag_cloud" == "true" ]; then
+  cd $WORKSPACE/geoprism-cloud
+  
+  git checkout master
+  git merge origin/dev
+  
+  if [ "$dry_run" == "false" ]; then
+    git push
+    git tag -a idm-$IDM_VERSION -m "Deployment scripts for IDM version $IDM_VERSION"
+    git push origin idm-$IDM_VERSION
+  else
+    git tag -a idm-$IDM_VERSION -m "Deployment scripts for IDM version $IDM_VERSION"
+  fi
+fi
+
+if [ "$release_github" == "true" ]; then
+  cd $WORKSPACE/uasdm
+  
+  gh config set prompt disabled
+  
+  if [ "$dry_run" == "false" ]; then
+    # TODO : We really should be using the artifacts we compiled earlier.
+    sleep 180 # Cloudsmith takes a little bit to process the artifact before its downloadable.
+    
+    wget https://dl.cloudsmith.io/public/terraframe/osmre-uav/maven/gov/osmre/uasdm/uasdm-web/$IDM_VERSION/uasdm-web-$IDM_VERSION.war -O uasdm-web-$IDM_VERSION.war
+    gh release create $IDM_VERSION "uasdm-web-$IDM_VERSION.war#Webapp War Artifact"
+  fi
+fi
