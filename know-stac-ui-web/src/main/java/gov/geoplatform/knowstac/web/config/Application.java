50a50b268c15d21081a7fab7e3e6edbe9536a52d
jsmethie <jsmethie@terraframe.com>
2026-02-23 12:47:00 -0700

    chore: revert of ui configuration

--- know-stac-ui-web/src/main/java/gov/geoplatform/knowstac/web/config/Application.java
+++ know-stac-ui-web/src/main/java/gov/geoplatform/knowstac/web/config/Application.java
@@ -2,22 +2,15 @@ package gov.geoplatform.knowstac.web.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "gov.geoplatform.knowstac.core", "gov.geoplatform.knowstac.web" })
 public class Application extends SpringBootServletInitializer
 {
   public static void main(String[] args)
   {
     SpringApplication.run(Application.class, args);
   }
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
-  {
-    return application.sources(WebConfiguration.class, WebMvcAutoConfiguration.class);
-  }
-
 }
